afda265d256b02cc3c3e72f069f10e39c43114a2
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-08-20 12:39:13 +1000

    map completed

--- src/model/Map.java
+++ src/model/Map.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Map {
+    private Entity map[][];
+
+    public Map() {
+        map = new Entity[9][9];
+        addEdgesAndWalls();
+    }
+
+    //adding edges and walls to the map
+    private void addEdgesAndWalls() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (i == 0 || j == 0 || i == 8 || j == 8 || i == 4 || j == 4) map[i][j] = new Cell(i, j);
+                else map[i][j] = new Wall(i, j);
+            }
+        }
+    }
+
+    public Entity[][] getMap() {
+        return map;
+    }
+
+    public void drawMap() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j].draw();
+            }
+        }
+    }
 }
