bf7c89359eee2479f55ce70ed38ec5e604bfb9a7
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-08-21 10:20:12 +1000

    player movement added

--- src/model/Player.java
+++ src/model/Player.java
@@ -10,6 +10,15 @@ public class Player extends Movable {
     @Override
     public void move(Map map, int xCoordinateOld, int yCoordinateOld, int xCoordinateNew, int yCoordinateNew) {
 
+        // moving player forward on the map
+        map.getMap()[xCoordinateNew][yCoordinateNew] = map.getMap()[xCoordinateOld][yCoordinateOld];
+
+        //setting new coordinates of the player
+        this.setxCorrdinate(xCoordinateNew);
+        this.setyCoordinate(yCoordinateNew);
+
+        //making old position of player a cell again
+        map.getMap()[xCoordinateOld][yCoordinateOld] = new Cell(xCoordinateOld,yCoordinateOld);
     }
 
     @Override
