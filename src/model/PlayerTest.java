7c76f077339d383583b85eb454d2a2642ef678f8
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-08-21 19:29:32 +1000

    unit test PlayerTest added

---
+++ src/model/PlayerTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
