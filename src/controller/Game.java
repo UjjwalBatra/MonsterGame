6e1150d010fb95b691fa94bf1f61c160939c6fa3
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-08-22 22:29:18 +1000

    makeing program runnable with test cases and main method.

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -14,7 +14,7 @@ public class Game {
         Map map = new Map();
         Movable player = new Player(map, 0,0);
         Movable monster = new Monster(map, 4,4);
-
+        System.out.println("testing");
 
     }
 }
--- src/model/PlayerTest.java
+++ src/model/PlayerTest.java
@@ -10,11 +10,23 @@ package model;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;class PlayerTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+class PlayerTest {
+
+    Movable player;
+    Map map;
     @BeforeEach
     void setUp() {
+        map = new Map();
+        player = new Player(map, 0, 0);
+    }
+
+    @Test
+    public void checkplayerposition(){
+        assertTrue(player.getxCorrdinate() == 1);
     }
 
     @AfterEach
