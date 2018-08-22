505981f2431393f0f3e29594f2c285348a7a897c
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-08-22 22:14:35 +1000

    PlayerTest modified

--- src/model/PlayerTest.java
+++ src/model/PlayerTest.java
@@ -1,14 +1,23 @@
+
+/*
+ *
+ * @project - MonsterGame
+ * @author - ujjwalbatra on 22/08/18
+ *
+ */
+
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
-class PlayerTest {
+import static org.junit.jupiter.api.Assertions.*;class PlayerTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
     }
 }
