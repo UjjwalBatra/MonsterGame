a55f891cf440b9d048772fc9c3a040fdf6958bc1
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-08-24 21:55:08 +1000

    restoring new controller design

---
+++ src/controller/KeyAction.java
@@ -0,0 +1,79 @@
+package controller;
+import model.Map;
+import model.Monster;
+import model.Movable;
+import model.Player;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyAction extends Frame implements KeyListener {
+
+    Label l;
+    TextArea area;
+    Movable player;
+    Movable monster;
+    Map map;
+
+    public KeyAction(Map map, Movable monster, Movable player)
+    {
+        this.map = map;
+        this.player = player;
+        this.monster = monster;
+
+        l = new Label();
+        l.setBounds(20,50,100,20);
+        area = new TextArea();
+        //area.SetBounds(20,80,300,300);
+        area.addKeyListener(this);
+
+        add(l);
+        add(area);
+        setSize(400,400);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_LEFT)
+        {
+            player.move(map, "left");
+            //System.out.println("Left arrow pressed");
+            map.drawMap();
+
+        }
+        else if(key == KeyEvent.VK_RIGHT)
+        {
+            player.move(map, "right");
+            map.drawMap();
+
+        }
+        else if(key == KeyEvent.VK_UP)
+        {
+            player.move(map, "up");
+            map.drawMap();
+
+        }
+        else if(key == KeyEvent.VK_DOWN)
+        {
+            player.move(map, "down");
+            map.drawMap();
+
+        }
+
+    }
+
+    public void keyReleased(KeyEvent e)
+    {
+
+    }
+
+    public void keyTyped(KeyEvent e)
+    {
+
+    }
+
+}
