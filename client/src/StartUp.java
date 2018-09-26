4e0f8126df1579454e8e3e3556eccbcda5f4eab1
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-09-26 13:55:01 +1000

    ipaddress variable initialised

--- client/src/StartUp.java
+++ client/src/StartUp.java
@@ -12,7 +12,11 @@ import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+
+
 public class StartUp {
+
+    private static final String ipAddress = "//10.132.116.80";
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry();
@@ -21,20 +25,20 @@ public class StartUp {
                 System.setSecurityManager(new SecurityManager());
             }
 
-            RemotePlayer player2 = (RemotePlayer) Naming.lookup("//10.132.116.80:2097/Player2");
+            RemotePlayer player2 = (RemotePlayer) Naming.lookup(ipAddress + ":2097/Player2");
 
             player2.move("left");
             player2.move("left");
 //            player2.getMap().drawMap();
 
-            RemotePlayer player3 = (RemotePlayer) Naming.lookup("//10.132.116.80:2098/Player3");
+            RemotePlayer player3 = (RemotePlayer) Naming.lookup(ipAddress + ":2098/Player3");
 
             player3.move("right");
             player3.move("right");
 //            player3.getMap().drawMap();
 
 
-            RemotePlayer player4 = (RemotePlayer) Naming.lookup("//10.132.116.80:2099/Player4");
+            RemotePlayer player4 = (RemotePlayer) Naming.lookup(ipAddress + ":2099/Player4");
 
             player4.move("left");
             player4.move("left");
